7d6e36194d6a8c3e355e23e23460f14991a0403d
Shane Kennedy <shane.kennedy19@gmail.com>
2020-02-23 16:34:31 +0100

    Organize code in npm.el file

--- npm.el
+++ npm.el
@@ -8,41 +8,47 @@
 (require 'transient)
 
 (defconst package-json-file "package.json")
-(defconst npm-run-prefix "npm run")
 
-(defun get-npm-run-command (script-name)
-  "Construct the shell command for a given SCRIPT-NAME."
-  (concat npm-run-prefix " " script-name))
-
-(defun get-npm-scripts (project-dir)
-  "Function to parse package.json in the PROJECT-DIR to find npm scripts."
-  (cdr (assoc 'scripts (json-read-file (concat project-dir package-json-file)))))
-
+;; Common
 (defun get-project-dir ()
   "Function that determines the file path of the project root directory."
   (locate-dominating-file (buffer-file-name) package-json-file))
 
-(defun npm-choose-script ()
+
+;; NPM RUN
+(defconst npm-run-prefix "npm run")
+
+(defun npm-run--get-run-command (script-name)
+  "Construct the shell command for a given SCRIPT-NAME."
+  (concat npm-run-prefix " " script-name))
+
+(defun npm-run--get-scripts (project-dir)
+  "Function to parse package.json in the PROJECT-DIR to find npm scripts."
+  (cdr (assoc 'scripts (json-read-file (concat project-dir package-json-file)))))
+
+
+(defun npm-run--choose-script ()
   "Let user choose which script to run."
   (interactive)
-  (completing-read "Select script from list: " (get-npm-scripts (get-project-dir)) nil t))
+  (completing-read "Select script from list: " (npm-run--get-scripts (get-project-dir)) nil t))
 
-(defun npm-run-command (prefix-command &optional args)
+(defun npm-run--command (prefix-command &optional args)
   "Invoke the compile mode with the test PREFIX-COMMAND and ARGS if provided."
   (interactive (list (npm-arguments)))
   (save-excursion
     (let* ((project-root-folder (find-file-noselect (get-project-dir)))
-          (command (get-npm-run-command (npm-choose-script))))
+          (command (npm-run--get-run-command (npm-run--choose-script))))
       (setq compilation-read-command t)
       (set-buffer project-root-folder)
       (setq compile-command command)
       (call-interactively 'compile)
       (kill-buffer project-root-folder))))
 
+;; Transient menu
 (define-transient-command npm ()
   "Open npm transient menu pop up."
     [["Command"
-    ("r" "Run"       npm-run-command)]]
+    ("r" "Run"       npm-run--command)]]
   (interactive)
   (transient-setup 'npm))
 
